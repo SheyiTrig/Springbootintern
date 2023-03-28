b4103bb494c9da4451976c36551510efbbf7c0c3
Adeyemo Oluwaseyifunmi <heasylife1234@gmail.com>
2023-03-28 14:39:54 +0100

    Add files via upload

---
+++ spring boot/customMessage.php
@@ -0,0 +1,20 @@
+<?php
+ function ErrorMsg1(){
+    echo"Data has not been fetcehd yet";
+ }
+
+
+
+ function ErrorMsg2(){
+    echo"Data has not been inserted yet";
+ }
+
+ function ErrorMsg3(){
+    echo"Data has not been inserted yet";
+ }
+
+ function SuccessMsg(){
+    echo"Data has been inserted";
+ }
+
+?>
